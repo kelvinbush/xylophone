7b290de9a70715da19d28920f5d3dde188d17350
Kelvin Bush <kelybush@gmail.com>
2021-07-09 13:41:51 +0300

    updating UI

--- lib/main.dart
+++ lib/main.dart
@@ -10,82 +10,99 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
+              Expanded(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  child: Text(
+                    "Click Me",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () => playSound(1),
                 ),
-                child: Text(
-                  "Click Me",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () => playSound(1),
               ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.orange),
+              Expanded(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.orange),
+                  ),
+                  child: Text(
+                    "Click Me",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () => playSound(2),
                 ),
-                child: Text(
-                  "Click Me",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () => playSound(2),
               ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.yellow),
+              Expanded(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.yellow),
+                  ),
+                  child: Text(
+                    "Click Me",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () => playSound(3),
                 ),
-                child: Text(
-                  "Click Me",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () => playSound(3),
               ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.green),
+              Expanded(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                  ),
+                  child: Text(
+                    "Click Me",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () => playSound(4),
                 ),
-                child: Text(
-                  "Click Me",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () => playSound(4),
               ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.teal),
+              Expanded(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.teal),
+                  ),
+                  child: Text(
+                    "Click Me",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () => playSound(5),
                 ),
-                child: Text(
-                  "Click Me",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () => playSound(5),
               ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.purple),
+              Expanded(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.purple),
+                  ),
+                  child:
+                      Text("Click Me", style: TextStyle(color: Colors.white)),
+                  onPressed: () => playSound(6),
                 ),
-                child: Text("Click Me", style: TextStyle(color: Colors.white)),
-                onPressed: () => playSound(6),
               ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.pink),
+              Expanded(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.pink),
+                  ),
+                  child: Text(
+                    "Click Me",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () => playSound(7),
                 ),
-                child: Text(
-                  "Click Me",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () => playSound(7),
               ),
             ],
           ),
