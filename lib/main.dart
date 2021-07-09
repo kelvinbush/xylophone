a1c0821a7b0b5f0e9f2173558db1f54431d30309
Kelvin Bush <kelybush@gmail.com>
2021-07-09 13:28:48 +0300

    play note 1 to 7

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,84 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-              child: TextButton(
-            child: Text("Click Me"),
-            onPressed: () => AudioCache().play('note1.wav'),
-          )),
+          child: Column(
+            children: [
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                child: Text(
+                  "Click Me",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => AudioCache().play('note1.wav'),
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.orange),
+                ),
+                child: Text(
+                  "Click Me",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => AudioCache().play('note2.wav'),
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.yellow),
+                ),
+                child: Text(
+                  "Click Me",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => AudioCache().play('note3.wav'),
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                ),
+                child: Text(
+                  "Click Me",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => AudioCache().play('note4.wav'),
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.teal),
+                ),
+                child: Text(
+                  "Click Me",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => AudioCache().play('note5.wav'),
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.purple),
+                ),
+                child: Text("Click Me", style: TextStyle(color: Colors.white)),
+                onPressed: () => AudioCache().play('note6.wav'),
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.pink),
+                ),
+                child: Text(
+                  "Click Me",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => AudioCache().play('note7.wav'),
+              ),
+            ],
+          ),
         ),
       ),
     );
