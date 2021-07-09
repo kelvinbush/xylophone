25283e5b2617bd0237bc53103e66694b2ede10a2
Kelvin Bush <kelybush@gmail.com>
2021-07-09 14:03:06 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,25 +20,23 @@ class XylophoneApp extends StatelessWidget {
       );
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              buildKey(Colors.blue, 1),
-              buildKey(Colors.orange, 2),
-              buildKey(Colors.yellow, 3),
-              buildKey(Colors.green, 4),
-              buildKey(Colors.teal, 5),
-              buildKey(Colors.purple, 6),
-              buildKey(Colors.pink, 7),
-            ],
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildKey(Colors.blue, 1),
+                buildKey(Colors.orange, 2),
+                buildKey(Colors.yellow, 3),
+                buildKey(Colors.green, 4),
+                buildKey(Colors.teal, 5),
+                buildKey(Colors.purple, 6),
+                buildKey(Colors.pink, 7),
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
